343806cc0ab1183a7c686655a5e0117c63aec5ab
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2025-06-24 15:22:14 +0400

    checkseeds: tool to check specific nodes.

---
+++ eslint.config.cjs
@@ -0,0 +1,40 @@
+'use strict';
+
+const rc = require('bslintrc');
+
+module.exports = [
+  rc.configs.recommended,
+  rc.configs.bcoin,
+  {
+    languageOptions: {
+      globals: {
+        ...rc.globals.node
+      },
+      ecmaVersion: 'latest'
+    }
+  },
+  {
+    files: [
+      'bin/hsd-tools',
+      '**/*.js',
+      '*.js'
+    ],
+    languageOptions: {
+      sourceType: 'commonjs'
+    }
+  },
+  {
+    files: ['test/{,**/}*.{js,cjs,mjs}'],
+    languageOptions: {
+      globals: {
+        ...rc.globals.mocha,
+        register: 'readable'
+      }
+    },
+    rules: {
+      'max-len': 'off',
+      'prefer-arrow-callback': 'off',
+      'no-return-assign': 'off'
+    }
+  }
+];
